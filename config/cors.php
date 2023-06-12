70c049961181897cdcf1db313a1c99e9256b6aa3
dipankar kataki <dipankar.kataki@ekodus.com>
2023-06-12 23:07:40 +0530

    cors fixing

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://admin.ekodus.ca/','*'],
+    'allowed_origins' => ['https://admin.ekodus.ca/*','*'],
 
     'allowed_origins_patterns' => [],
 
